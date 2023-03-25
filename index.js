8cdec45d11195a341a19c09ebd2512d68df3db0a
Kirti Shinde <kirti182001@gmail.com>
2023-03-25 10:14:11 +0530

    test

---
+++ index.js
@@ -0,0 +1,43 @@
+// index.js
+
+//  import the crypto module
+
+const arg = process.argv;
+const crypto = require("crypto");
+
+//  get a commands using process.argv
+let num1= arg[3]
+let num2 = arg[4]
+
+// complete the  function
+if(arg[2]=="sum"){
+  console.log("additon is",+num1 + +num2)
+}
+else if(arg[2]=="mul"){
+  console.log("multiplication is",+num1 * +num2)
+}
+else if(arg[2]=="div"){
+  console.log("division is",+num1 / +num2)
+}
+else if(arg[2]=="sub"){
+  console.log("substraction is",+num1 - +num2)
+}
+else if(arg[2]=="sin"){
+  console.log("sin value is",Math.sin(+num1))
+}
+else if(arg[2]=="cos"){
+  console.log("cos value is",Math.cos(+num1))
+}
+else if(arg[2]=="tan"){
+  console.log("tan value is",Math.tan(+num1))
+}
+else if(arg[2]=="random"){
+  console.log("random number between(30-50)",crypto.randomInt(30, 50));
+}
+
+
+switch (operation) {
+  
+  default:
+    console.log("Invalid operation");
+}
